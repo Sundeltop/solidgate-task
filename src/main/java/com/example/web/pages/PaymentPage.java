c40cad0c54ddbb97b2d8b34c0ff34b062aaa2ce1
Oleksandr Nelipa <oleksandr.nelipa1@gmail.com>
2024-08-24 21:17:37 +0300

    Updated PaymentPage with new methods

--- src/main/java/com/example/web/pages/PaymentPage.java
+++ src/main/java/com/example/web/pages/PaymentPage.java
@@ -2,8 +2,11 @@ package com.example.web.pages;
 
 import com.codeborne.selenide.SelenideElement;
 
+import java.util.regex.Matcher;
+
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
+import static java.util.regex.Pattern.compile;
 
 public class PaymentPage {
 
@@ -12,19 +15,31 @@ public class PaymentPage {
     private final SelenideElement cvcInput = $("[data-testid='cardCvv']");
     private final SelenideElement nameOnCardInput = $("[data-testid='cardHolder']");
     private final SelenideElement submitPaymentBtn = $("button[data-testid='submit']");
+    private final SelenideElement loadingSpinner = $(".CardForm_spinner__oFYh8");
+    private final SelenideElement orderPrice = $("[data-testid='price_major']");
 
     public PaymentPage setCardNumber(String cardNumber) {
         cardNumberInput
                 .shouldBe(visible, editable)
-                .setValue(cardNumber);
+                .setValue(cardNumber)
+                .shouldHave(value(cardNumber.replaceAll("(.{4})", "$1 ")));
 
         return this;
+    }
+
+    public String getCurrency() {
+        return parseOrderPrice().group(1);
+    }
+
+    public String getOrderAmount() {
+        return parseOrderPrice().group(2);
     }
 
     public PaymentPage setCardExpiry(String cardExpiry) {
         cardExpiryInput
                 .shouldBe(visible, editable)
-                .setValue(cardExpiry);
+                .setValue(cardExpiry)
+                .shouldHave(value(cardExpiry));
 
         return this;
     }
@@ -32,7 +47,8 @@ public class PaymentPage {
     public PaymentPage setCvc(String cvc) {
         cvcInput
                 .shouldBe(visible, editable)
-                .setValue(cvc);
+                .setValue(cvc)
+                .shouldHave(value(cvc));
 
         return this;
     }
@@ -49,5 +65,23 @@ public class PaymentPage {
         submitPaymentBtn
                 .shouldBe(visible, clickable)
                 .click();
+
+        loadingSpinner.shouldNotBe(visible);
+    }
+
+    private Matcher parseOrderPrice() {
+        final String regex = "([€£$])([0-9,.]+)";
+
+        final String currencyWithAmount = orderPrice
+                .shouldBe(visible)
+                .text();
+
+        final Matcher matcher = compile(regex).matcher(currencyWithAmount);
+
+        if (matcher.matches()) {
+            return matcher;
+        }
+
+        throw new RuntimeException("Error occurred during parsing order price");
     }
 }
